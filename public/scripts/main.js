b5a3823e3b0cc1b24725dc19bcc0e684641b10d8
Daniel Dvorkin <danieldvorkin@Daniels-MacBook-Pro.local>
2016-10-13 16:18:05 -0400

    got the app fully working, now time to add Add/Edit/Remove product functionality

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,53 +1,78 @@
+// Parent Component: ProductCategoryRow <- ProductTable <- FilterableProductTable
+var ProductItem = React.createClass({
+  render: function(){
+    return (
+      <div className="product">
+        {this.props.product.name}
+      </div>
+    )
+  }
+});
+
+// Parent Component: ProductTable <- FilterableProductTable
 var ProductCategoryRow = React.createClass({
   render: function(){
+    var products = this.props.products
+    if(this.props.checked){
+      products = products.filter(function(value){
+        return value.stocked
+      });
+    }
+    var category = this.props.category
     return (
       <div className="productCategoryRow">
-        {{this.props.category}}
+        <h1>{this.props.category}</h1>
+        {products.map((product) => {
+          if(product.category == category && product.name.includes(this.props.query)){
+            return <ProductItem product={product} key={product.id} />  
+          }
+        })}
       </div>
     )
   }
 });
 
+// Parent Component: FilterableProductTable
 var ProductTable = React.createClass({
   render: function(){
-    var categories = this.props.products.map(function(product){
-      return (
-        <ProductCategoryRow category={product.category}/>
-      )
-    });
+    var categories = this.props.products.map((product) =>
+      product.category
+    ).reduce(function(a,b){if(a.indexOf(b)<0)a.push(b);return a;},[]);
+
     return (
       <div className="productTable">
-       {categories}
+        {categories.map((category, index) =>
+          <ProductCategoryRow category={category} key={index} products={this.props.products} checked={this.props.checked} query={this.props.query} />
+        )}
       </div>
     )
   }
 });
 
+// Parent Component: Navbar <- FilterableProductTable
 var NavBarForm = React.createClass({
   getInitialState: function(){
-    return { query: '' };
+    return { query: '', checked: false };
   },
   handleQueryChange: function(e){
-    this.setState({query: e.target.value});
+    this.setState({query: e.target.value}, function(){ this.props.onClick(this.state.query) });
   },
-  handleSubmit: function(e){
-    e.preventDefault();
-
-    this.props.onClick({query: this.state.query.toString()})
-    this.setState({query: ''})
+  toggleCheckboxChange: function(){
+    this.props.onCheckbox()
   },
   render: function(){
     return (
-      <form className="navBarForm navbar-form navbar-left" onSubmit={this.handleSubmit}>
+      <form className="navBarForm navbar-form navbar-left">
         <div className="form-group">
-          <input type="text" className="form-control search-bar" placeholder="Search" value={this.state.query} onChange={this.handleQueryChange}/>
+          <input type="text" className="form-control search-bar" placeholder="Search" onChange={this.handleQueryChange} />
         </div>
-        <button type="submit" className="btn btn-default submit-btn">Submit</button>
+        <input type="checkbox" id="inStock" checked={this.props.checked} onChange={this.toggleCheckboxChange} /> Show in stock only
       </form>
     )
   }
 });
 
+// Parent Component: Navbar <- FilterableProductTable
 var NavBarBrand = React.createClass({
   render: function(){
     return (
@@ -64,9 +89,13 @@ var NavBarBrand = React.createClass({
   }
 });
 
+// Parent Component: FilterableProductTable
 var NavBar = React.createClass({
-  searchSubmit: function(){
-    this.props.onClick();
+  searchSubmit: function(query){
+    this.props.onClick(query);
+  },
+  showInStock: function(){
+    this.props.onCheckboxClick();
   },
   render: function(){
     return (
@@ -75,7 +104,7 @@ var NavBar = React.createClass({
           <div className="container">
             <NavBarBrand/>       
             <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-              <NavBarForm onClick={this.searchSubmit}/>
+              <NavBarForm onClick={this.searchSubmit} onCheckbox={this.showInStock} checked={this.props.checked} />
             </div>
           </div>
         </nav>
@@ -85,29 +114,51 @@ var NavBar = React.createClass({
 });
 
 var FilterableProductTable = React.createClass({
-  searchSubmit: function(){
-    console.log("Oh Shiiiitt")
+  loadData: function(){
+    $.ajax ({
+      url: this.props.url,
+      dataType: 'json',
+      cache: false,
+      success: function(data){
+        this.setState({data: data})
+      }.bind(this),
+      error: function(xhr, status, err){
+        console.log(this.props.url, status, err.toString());
+      }.bind(this)
+    });
+  },
+  getInitialState: function(){
+    return {data: [], query: '', checked: false};
+  },
+  componentDidMount: function(){
+    this.loadData();
+    setInterval(this.loadData, this.props.poll);
+  },
+  searchSubmit: function(query){
+    this.setState({
+      query: query
+    }, function(){
+      console.log("Query State: ", this.state.query)
+    });
+  },
+  showInStock: function(){
+    this.setState({
+      checked: !this.state.checked
+    }, function(){
+
+    });
   },
   render: function(){
     return (
       <div className="filterableProductTable">
-        <NavBar onClick={this.searchSubmit}/>
-        <ProductTable products={this.props.products}/>
+        <NavBar onClick={this.searchSubmit} onCheckboxClick={this.showInStock} checked={this.state.checked} />
+        <ProductTable products={this.state.data} checked={this.state.checked} query={this.state.query} />
       </div>
     )
   }
 });
 
-var PRODUCTS = [
-  {category: 'Sporting Goods', price: '$49.99', stocked: true, name: 'Football'},
-  {category: 'Sporting Goods', price: '$9.99', stocked: true, name: 'Baseball'},
-  {category: 'Sporting Goods', price: '$29.99', stocked: false, name: 'Basketball'},
-  {category: 'Electronics', price: '$99.99', stocked: true, name: 'iPod Touch'},
-  {category: 'Electronics', price: '$399.99', stocked: false, name: 'iPhone 5'},
-  {category: 'Electronics', price: '$199.99', stocked: true, name: 'Nexus 7'}
-];
-
 ReactDOM.render(
-  <FilterableProductTable products={PRODUCTS}/>,
+  <FilterableProductTable url="/api/data" poll={2000} />,
   document.getElementById('content')
 )
